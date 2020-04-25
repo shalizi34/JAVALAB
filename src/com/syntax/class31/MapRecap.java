6855385ccfaf3503f0e678928b5837c05d1469e5
shalizi34 <shalizi.shaliz@gmail.com>
2020-04-25 11:41:22 -0400

    Map Recap

---
+++ src/com/syntax/class31/MapRecap.java
@@ -0,0 +1,18 @@
+package com.syntax.class31;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapRecap {
+
+	
+	public static void main (String[] args) {
+		Map<String, String> hmap=new HashMap<>();
+		hmap.put("Afghanistan", "Kabul");
+		hmap.put("USA", "Washington DC");
+		hmap.put("France", "Paris");
+		hmap.put("Italy", "Rome");
+		System.out.println(hmap);
+		
+	}
+}
